2ddcd25c1cfb985c16383055ea57f06716cdeda3
Dmitriy Bazanov <zugzvangg1@gmail.com>
2023-10-31 16:33:17 +0300

    Update

--- tpos/HW2/filler/fill.py
+++ tpos/HW2/filler/fill.py
@@ -2,19 +2,22 @@ import mariadb
 import pandas as pd
 import os
 
-TABLE_NAME = "TEST"
-
+TABLE_NAME = os.getenv("TABLE_NAME")
 if __name__ == "__main__":
     # Connect to MariaDB 
     conn = mariadb.connect(
         host=os.getenv('DB_HOST'),
-        user="user",
-        password="password",
-        database="database"
+        user=os.getenv("MARIADB_USER"),
+        password=os.getenv("MARIADB_PASSWORD"),
+        database=os.getenv("MARIADB_DATABASE")
     )
     cursor = conn.cursor()
-    cursor.execute(f"DROP TABLE IF EXISTS {TABLE_NAME}")
-    cursor.execute(f"CREATE TABLE {TABLE_NAME}  (name VARCHAR(50), age INT)")
+    # cursor.execute(f"DROP TABLE IF EXISTS {TABLE_NAME}")
+    try:
+        cursor.execute(f"CREATE TABLE {TABLE_NAME}  (name VARCHAR(50), age INT)")
+    except mariadb.OperationalError:
+        print(f"Table {TABLE_NAME} already exists, fill it")
+        pass
 
     data = pd.read_csv("data.csv")
     # Write info to DB
@@ -24,6 +27,7 @@ if __name__ == "__main__":
         (row["name"], row["age"]))
 
     #Check that info is in table
+    print("Check that db is filled:")
     cursor.execute(f"SELECT * FROM {TABLE_NAME}")
     for (name, age) in cursor:
         print(f"Name: {name}, age: {age}")
