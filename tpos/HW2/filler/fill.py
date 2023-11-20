7cdb440f0f3ada9843f84a1f5ce1142f23430614
Dmitriy Bazanov <zugzvangg1@gmail.com>
2023-11-20 21:50:01 +0300

    Working hw2 tpos

--- tpos/HW2/filler/fill.py
+++ tpos/HW2/filler/fill.py
@@ -1,39 +1,58 @@
-import mariadb
 import pandas as pd
 import os
-import time
+import pymysql
 
 TABLE_NAME = os.getenv("TABLE_NAME")
+MARIADB_DATABASE = os.getenv("MARIADB_DATABASE")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv('DB_PORT'))
+MARIADB_ROOT_USER = os.getenv("MARIADB_ROOT_USER")
+MARIADB_ROOT_PASSWORD = os.getenv("MARIADB_ROOT_PASSWORD")
+# MYSQL_ROOT_USER = os.getenv("MYSQL_ROOT_USER")
+# MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
+
+def get_connection():
+    try:
+        connection = pymysql.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=MARIADB_ROOT_USER,
+            password=MARIADB_ROOT_PASSWORD,
+        )
+        cursor = connection.cursor()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {MARIADB_DATABASE}")
+        cursor.execute(f"USE {MARIADB_DATABASE}")
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (name VARCHAR(50), age INTEGER)")
+        cursor.execute("FLUSH TABLES")
+    except Exception as e:
+        raise e
+    return connection, cursor
+
 
 if __name__ == "__main__":
-    # Connect to MariaDB
-    time.sleep(5)
-    conn = mariadb.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("MARIADB_USER"),
-        password=os.getenv("MARIADB_PASSWORD"),
-        database=os.getenv("MARIADB_DATABASE"),
-    )
-    cursor = conn.cursor()
-    # cursor.execute(f"DROP TABLE IF EXISTS {TABLE_NAME}")
-    try:
-        cursor.execute(f"CREATE TABLE {TABLE_NAME}  (name VARCHAR(50), age INT)")
-    except mariadb.OperationalError:
-        print(f"Table {TABLE_NAME} already exists, fill it")
-        pass
-
+    connection, cursor = get_connection()
     data = pd.read_csv("data.csv")
     # Write info to DB
     for index, row in data.iterrows():
         cursor.execute(
-            f"INSERT INTO {TABLE_NAME} (name, age) VALUES (?, ?)",
+            f"INSERT INTO {TABLE_NAME} (name, age) VALUES (%s, %s)",
             (row["name"], row["age"]),
         )
-    conn.commit()
     # Check that info is in table
     print("Check that db is filled:")
     cursor.execute(f"SELECT * FROM {TABLE_NAME}")
-    for name, age in cursor:
-        print(f"Name: {name}, age: {age}")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
     # Close connection
-    conn.close()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    
+
+
+
+
+
+
+
--- tpos/HW2/web/web.py
+++ tpos/HW2/web/web.py
@@ -1,34 +1,46 @@
 from flask import Flask
-import mariadb
 import os
-import requests
+import pymysql
+
 
 TABLE_NAME = os.getenv("TABLE_NAME")
-
+MARIADB_DATABASE = os.getenv("MARIADB_DATABASE")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv('DB_PORT'))
+MARIADB_ROOT_USER = os.getenv("MARIADB_ROOT_USER")
+MARIADB_ROOT_PASSWORD = os.getenv("MARIADB_ROOT_PASSWORD")
+# MYSQL_ROOT_USER = os.getenv("MYSQL_ROOT_USER")
+# MYSQL_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
 
 def get_connection():
-    conn = mariadb.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("MARIADB_USER"),
-        password=os.getenv("MARIADB_PASSWORD"),
-        database=os.getenv("MARIADB_DATABASE"),
-    )
-    return conn
-
+    try:
+        connection = pymysql.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=MARIADB_ROOT_USER,
+            password=MARIADB_ROOT_PASSWORD,
+        )
+        cursor = connection.cursor()
+        cursor.execute(f"USE {MARIADB_DATABASE}")
+        cursor.execute("FLUSH TABLES")
+    except Exception as e:
+        raise e
+    return connection, cursor
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def all_data():
-    cursor = get_connection().cursor()
+    connection, cursor = get_connection()
     cursor.execute(f"SELECT * FROM {TABLE_NAME}")
-    result = {"result": []}
-    for name, age in cursor:
-        result["result"].append({"name": name, "age": age})
-    return result
-
+    results = cursor.fetchall()
+    json_res = {"result": []}
+    for name, age in results:
+        json_res["result"].append({name: age})
+    connection.close()
+    return json_res
 
 @app.route("/health")
 def health():
     return {"status": "OK"}
+
